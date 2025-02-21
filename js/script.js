cf4620e4848f99298b04962771a863396c8046fb
Charanraju18 <charanraju925@gmail.com>
2025-02-21 12:16:51 +0530

    Refactor CSS and JavaScript code
    - Removed unnecessary console.log statement in main.js
    - Fixed list styling in style.css
    - Added cursor pointer to nav-link hover in style.css
    - Updated background image in index.html
    - Updated social media links in index.html

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-// document
-//   .getElementById("contactForm")
-//   .addEventListener("submit", async function (e) {
-//     e.preventDefault();
-
-//     const name = document.getElementById("name").value;
-//     const email = document.getElementById("email").value;
-//     const subject = document.getElementById("subject").value;
-//     const message = document.getElementById("message").value;
-
-//     const response = await fetch(
-//       "https://portfolio-backend-fk8o.onrender.com/send-email", // Add "/send-email"
-//       {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({ name, email, subject, message }),
-//       }
-//     );
-
-//     const result = await response.json();
-//     alert(result.message);
-//   });
-
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector(".contact-form");
 
@@ -29,10 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
   popup.textContent = "Message sent successfully!";
   popup.style.cssText = `
     position: fixed;
-    top: 20px;
-    left: 50%;
+    top: 60px;
+    left: 78%;
     transform: translateX(-50%);
-    background-color: #28a745;
+    background-color: #FFBD39;
     color: white;
     padding: 12px 20px;
     border-radius: 5px;
